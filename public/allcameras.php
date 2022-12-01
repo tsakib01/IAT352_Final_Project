e8e3b4d52e1ccd6ed64dd43eeb86f77cadebb005
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-11-30 22:59:01 -0800

    queries the db to obtain all cameras

--- public/allcameras.php
+++ public/allcameras.php
@@ -1,1 +1,20 @@
 <!-- Shows all cameras  -->
+
+<?php
+    require_once("../private/database.php");
+    require_once("../private/included_functions.php");
+
+    no_SSL(); // switches to http
+
+    $selectQuery = "SELECT * FROM cameras";
+    $result = $db->query($selectQuery);
+
+    while ($row = $result->fetch_row()) {
+        format_model_name_as_link($row[0], $row[1],"modeldetails.php");
+        echo "<br/>";
+    };
+
+    $result->free_result();
+    $db->close();
+
+?>
