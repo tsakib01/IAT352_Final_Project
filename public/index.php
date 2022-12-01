dcc49ef513b7d64c18cf1fd3482a585ead962602
iero525 <119400104+iero525@users.noreply.github.com>
2022-12-01 06:21:42 -0800

    updated with header and footer

--- public/index.php
+++ public/index.php
@@ -1,2 +1,6 @@
 <!-- INDEX PAGE  -->
 <!-- CHECK header.php and footer.php IN PRIVATE/SHARED DIRECTORY -->
+<?php
+require("../private/shared/public_header.php");
+require("../private/shared/public_footer.php");
+?>
