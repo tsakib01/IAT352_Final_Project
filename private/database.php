0134f6f9ad90f5d0f2aebecdf832b27021286bb5
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-11-30 22:32:12 -0800

    added functions for connecting db

--- private/database.php
+++ private/database.php
@@ -1,1 +1,21 @@
 <!-- Database related functions goes here -->
+<?php
+    require_once('database_credentials.php');
+
+    // returns connection to the database if credentials are correct otherwise displays an error
+    function connectToDB($dbhost, $dbuser, $dbpass, $dbname) {
+        $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+        //did connection occur database?
+        if (mysqli_connect_errno()) {
+            //quit and display error and error number
+            die("Database connection failed:" .
+                mysqli_connect_error() .
+                " (" . mysqli_connect_errno() . ")"
+            );
+        }
+        return $connection;
+    }
+
+    $db = connectToDB($dbhost, $dbuser, $dbpass, $dbname);
+
+?>
--- private/database_credentials.php
+++ private/database_credentials.php
@@ -1,1 +1,7 @@
 <!-- Database server, host, name, password goes here -->
+<?php
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "grabacam";
+?>
