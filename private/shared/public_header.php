a2b3545be97ab8c3f94c55bf79f9483722168058
iero525 <119400104+iero525@users.noreply.github.com>
2022-12-01 06:22:17 -0800

    updated header

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -4,34 +4,34 @@
     <title>Camera Renting Website</title>
     <style>
     </style>
-    <link rel="stylesheet" type="text/css" href="../../public/css/main.css">
-    <link rel="stylesheet" type="text/css" href="../../public/css/theme.css">
-    <script src="../../public/js/index.js"></script>
-    <script src="../../public/js/jquery-3.6.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="../public/css/main.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/theme.css">
+    <script src="../public/js/index.js"></script>
+    <script src="../public/js/jquery-3.6.1.js"></script>
 </head>
 
 <body>
     <header>
-        <img src="../../352-LOGO.png" width="70" height="70" alt="logo" style="float: left">
+        <img src="../352-LOGO.png" width="70" height="70" alt="logo" style="float: left">
         <nav class="title-large">
-            <a href="../../public/allcameras.php">All Cameras</a>
-            <a href="../../public/showwatchlist.php">Watch List</a>
-            <a href="../../public/rentals.php">Rental History</a>
-            <a href="../../public/about.php">About Us</a>
+            <a href="../public/allcameras.php">All Cameras</a>
+            <a href="../public/showwatchlist.php">Watch List</a>
+            <a href="../public/rentals.php">Rental History</a>
+            <a href="../public/about.php">About Us</a>
         </nav>
     </header>
 
     <div class="tooltip">
-        <img src="../../352-USER.png" alt="user" width="30" height="30" alt="logo">
+        <img src="../352-USER.png" alt="user" width="30" height="30" alt="logo">
         <div class="tooltiptext">
             <p class="headline-small">My Account</p>
             <?php
             if (isset($_SESSION['valid_user']))
-                echo "<p class=\"title-medium\"><a href=\"../../public/user.php\">Manage Account</a></p>
-                    <p class=\"title-medium\"><a href=\"../../public/signout.php\">Sign Out</a></p>";
+                echo "<p class=\"title-medium\"><a href=\"../public/user.php\">Manage Account</a></p>
+                    <p class=\"title-medium\"><a href=\"../public/signout.php\">Sign Out</a></p>";
             else
-                echo "<p class=\"title-medium\"><a href=\"../../public/signup.php\">Create Account</a></p>
-                        <p class=\"title-medium\"><a href=\"../../public/login.php\">Sign In</a></p>";
+                echo "<p class=\"title-medium\"><a href=\"../public/signup.php\">Create Account</a></p>
+                        <p class=\"title-medium\"><a href=\"../public/login.php\">Sign In</a></p>";
             ?>
         </div>
     </div>
