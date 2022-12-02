f84d5583844ee14fb4f56726ed138cb25d2a8847
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-12-01 17:20:47 -0800

    allcameras page completed

--- public/allcameras.php
+++ public/allcameras.php
@@ -1,6 +1,7 @@
 <!-- Shows all cameras  -->
 
 <?php
+    session_start();
     require_once("../private/database.php");
     require_once("../private/included_functions.php");
 
@@ -9,10 +10,21 @@
     $selectQuery = "SELECT * FROM cameras";
     $result = $db->query($selectQuery);
 
+
+    require("../private/shared/public_header.php");
+
+    echo "<main>";
+    echo "<h1>Camera List</h1>";
+
+    echo "<div class='camera-list'>";
     while ($row = $result->fetch_row()) {
-        format_model_name_as_link($row[0], $row[1],"modeldetails.php");
-        echo "<br/>";
+        format_model_name_as_link($row[0], $row[1],"cameradetails.php","./images/agfaEphoto1280.PNG",$row[13]);
     };
+
+    echo "</div>";
+    echo "</main>";
+
+    require("../private/shared/public_footer.php");
 
     $result->free_result();
     $db->close();
