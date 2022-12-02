23213b0fc5c9de0f7f9fa455121a2d6a60334d94
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-12-02 01:31:16 -0800

    added some content in about

--- public/about.php
+++ public/about.php
@@ -1,38 +1,32 @@
-<!-- TODO: ABOUT TODO -->
-
-<!-- TODO: OUR STORY -->
-
-<!-- TODO: OUR GOALS -->
-
-<!-- TODO: CONTACT US -->
-
-<!-- TODO: FAQs -->
-
 <?php
+    session_start();
     include_once('../private/shared/public_header.php');
 ?>
 
 
-<main>
+<main class='about''>
     <section>
         <h2>Our Story</h2>
-        <p></p>
+        <p>Finding how expensive it is to obtain a top quality camera which people don't use everyday, we came up with the 
+            idea to help people afford a camera through rental to capture their beautiful memories.
+        </p>
     </section>
     <section>
         <h2>Our Goals</h2>
-        <p></p>
+        <p>Our primary goal is to help people with the camera that fits their needs. Whether you are a professional photographer or a regular person,
+            we got you covered.
+        </p>
     </section>
     <section>
         <h2>Contact Us</h2>
-        <p></p>
-    </section>
-    <section>
-        <h2>Contact Us</h2>
-        <p></p>
+        <p>Address: 1234 Main St, Vancouver, BC, Canada</p>
+        <p>Phone:   +1 777-777-0000</p>
+        <p>Email:   grabacam@grabacam.com</p>
     </section>
     <section>
         <h2>FAQs</h2>
-        <p></p>
+        <p>Q. What if I lost or broke the camera?</p>
+        <p>A. You can have a buy a insurance while checking out to prevent getting charged for the camera. Or you can email the support to make an arrangement.</p>
     </section>
 </main>
 
