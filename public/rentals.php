c2bd342602d028fcac46603d8ff3e92345276faa
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-12-02 01:32:16 -0800

    implemented rentals page

--- public/rentals.php
+++ public/rentals.php
@@ -7,7 +7,32 @@
 
     include_once('../private/shared/public_header.php');
 
+    echo "<main>";
 
+    if(isset($_SESSION['valid_user'])){
+        $email = $_SESSION['valid_user'];
+        $selectQuery = "SELECT cameras.model, rents.fromDate, rents.toDate 
+                        FROM rents 
+                        INNER JOIN cameras 
+                        ON rents.cid=cameras.cid
+                        WHERE rents.email='$email'";
+
+        $result = $db->query($selectQuery);
+
+        while ($row = $result->fetch_row()) {
+            echo "<div class='rental-item'>";
+            echo "<h2>$row[0]</h2>";
+            echo "<p><strong>From :</strong> $row[1]</p>";
+            echo "<p><strong>To :</strong> $row[2]</p>";
+            echo "</div>";
+        }
+    }
+    else {
+        $_SESSION['callback_url'] = 'rentals.php';
+        redirect_to('login.php');
+    }
+
+    echo "</main>";
 
     include_once('../private/shared/public_footer.php');
 
