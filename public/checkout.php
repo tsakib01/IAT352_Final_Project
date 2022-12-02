485478694a41bf189b3bd063c47344059ce78fe2
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-12-02 01:32:02 -0800

    implemented checkout page

--- public/checkout.php
+++ public/checkout.php
@@ -6,9 +6,119 @@
 
     include_once('../private/shared/public_header.php');
 
+    echo "<main class='checkout'>";
+
+    if(isset($_GET['cid'])){
+        $code = trim($_GET['cid']); // gets the camera Id from redirected link
+    }
+
+    $isValid = true;
+    $email = "";
+    $cid = 0;
+    $from = "";
+    $to = "";
 
 
+
+    if (isset($_POST['checkout'])){
+        $email = $_SESSION['valid_user'];
+        $cid = $_POST['cid'];
+        $from = $_POST['from'];
+        $to = $_POST['to'];
+    }
+
+    if (isset($_POST['checkout'])){
+
+        if($from == ""){
+            echo "<p>From entry missing</p>";
+            $isValid = false;
+        }
+
+        if($to == ""){
+            echo "<p>To entry missing</p>";
+            $isValid = false;
+        }
+            
+        if($from > $to && $isValid) {
+            echo "<p>From can't be greater than to </p>";
+            $isValid = false;
+        }
+
+    }
+
+
+    // checks if already booked around that date
+    if (isset($_POST['checkout']) && $isValid){
+
+        $selectQuery = "SELECT * FROM rents 
+                        WHERE cid=$cid 
+                        AND '$from' BETWEEN fromDate AND toDate
+                        OR '$to' BETWEEN fromDate AND toDate";
+
+        $result = $db->query($selectQuery);
+        
+        $count = 0;
+
+        while($row = $result->fetch_row())
+            $count++;
+
+        $isValid = $count >= 0 ? false : true;
+
+        echo "<p>The camera is not available in that date range.</p>"; 
+        echo "<p>Try a different date range or a different camera.</p>";
+
+        $result->free_result();
+    }
+
+    // books the user
+    if (isset($_POST['checkout']) && $isValid){
+
+        $insertQuery = "INSERT INTO rents (email,cid,fromDate,toDate) VALUES (?,?,?,?)";
+        $stmt = $db->prepare($insertQuery);
+        $stmt->bind_param('sdss',$email,$cid,$from,$to);
+        $stmt->execute();
+
+        echo "<h1>Your booking has been successful.</h1>";
+
+    }
+
+    if(!isset($_POST['checkout'])){
+        $selectQuery = "SELECT C.model
+        FROM cameras C
+        WHERE C.cid = ?"; 
+        
+        $stmt = $db->prepare($selectQuery);
+        $stmt->bind_param('d',$code);
+        $stmt->execute();
+        $stmt->bind_result($model);
+
+
+        if($stmt->fetch()) {
+            echo "<h1>$model</h1>\n";
+        }
+
+        if(isset($_SESSION['valid_user'])) {
+
+            echo "<form action='checkout.php' method='post'>";
+
+            echo "<label><p>From</p>";
+            echo "<input type='date' name='from' placeholder='From'>";
+            echo "</label>";
+
+            echo "<label><p>To</p>";
+            echo "<input type='date' name='to' placeholder='To'>";
+            echo "</label>";
+
+            echo "<input type='hidden' name='cid' value='$code'>";
+
+            echo "<input id='checkout' type='submit' name='checkout' value='Check Out'>";
+            echo "</form>";
+        }
+    }
+
+    echo "</main>";
+
+    $db->close();
     include_once('../private/shared/public_footer.php');
-
 
 ?>
